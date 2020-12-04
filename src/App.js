f8b1293d297ebe01b762805726f3808008a7b6f6
Amartya Mondal <atm1504.in@gmail.com>
2020-12-05 00:58:57 +0530

    Initialized routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+import { Route, Switch } from "react-router-dom";
 import './App.css';
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 
 import { generatePalette} from "./colorHelpers";
+// import { Route } from 'react-router-dom';
 function App() {
   console.log(generatePalette(seedColors[4]));
   return (
-    <div>
-      <Palette palette={generatePalette(seedColors[4])}/>
-    </div>
+    <Switch>
+      <Route exact path="/" render={()=><h1>Palette list goes here</h1>} />
+      <Route exact path="/palette/:id" render={()=><h1>Individual palette</h1>} />
+      {/* <div>
+        <Palette palette={generatePalette(seedColors[4])}/>
+        </div> */}
+    </Switch>
   );
 }
 
