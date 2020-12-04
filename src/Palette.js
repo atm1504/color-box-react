a6b7d14e343c3bc637dae77eef2bc5d8b53e1827
Amartya Mondal <atm1504.in@gmail.com>
2020-12-04 12:03:25 +0530

    Simple colorr box

--- src/Palette.js
+++ src/Palette.js
@@ -1,13 +1,20 @@
- import React, { Component } from 'react';
+import React, { Component } from 'react';
+import ColorBox from "./ColorBox";
+import "./Palette.css";
+
 
 export default class Palette extends Component {
     render() {
+        const colorBoxes = this.props.colors.map(color => (
+            <ColorBox background={color.color} name={color.name} />
+        ));
         return (
             <div className="Palette">
                 <h1>Palette</h1>
                 {/* navbar here */}
                 <div className="Palette-colors">
-                    {/* bunch of colour boxes */ }
+                    {/* bunch of colour boxes */}
+                    {colorBoxes}
                 </div>
                 {/* Footer here */ }
             </div>
