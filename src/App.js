bc7287f89c94b5f06c2644f8dd88a77eadf52121
Amartya Mondal <atm1504.in@gmail.com>
2020-12-05 16:20:37 +0530

    add route for color

--- src/App.js
+++ src/App.js
@@ -17,7 +17,10 @@ class App extends Component{
     <Switch>
         <Route exact path="/"
           render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps} />} />
-      <Route exact path="/palette/:id" render={(routeProps)=><Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))}/>} />
+        <Route exact path="/palette/:id" render={(routeProps) => <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />} />
+        <Route exact
+          path="/palette/:paletteId/:colorId"
+          render={ ()=> <h1>Single color page</h1>}/>
     </Switch>
     );
   }
--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import "./ColorBox.css";
+import { Link, withRouter } from "react-router-dom";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
 export default class ColorBox extends Component {
@@ -22,21 +23,10 @@ export default class ColorBox extends Component {
     render() {
         const { name, background } = this.props;
         const { copied } = this.state;
-        // let over;
-        // if (copied) {
-        //     over = <div style={{ background }} className={"copy-overlay show"} />
-        //     console.log("Yes bro");
-        // } else {
-        //     over=<span></span>
-        // }
-        
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div style={{ background: background }} className="ColorBox">
-                    {/* {!copied?<div style={{background }} className={`copy-overlay`}/>:<div style={{background }} className={`copy-overlay show`}/>} */}
-                    {/* {copied?<div style={{ background: background }} className={`copy-overlay show`}/>:<span></span>} */}
                     {console.log(copied)}
-                    {/* {copied ? <div style={{ background }} className={"copy-overlay show"} /> : <span></span>} */}
                     <div className={`copy-overlay ${copied && "show"}`} style={{ background }}/>
                     <div className={`copy-msg ${copied && "show"}`}>
                         <h1>copied!</h1>
@@ -48,7 +38,9 @@ export default class ColorBox extends Component {
                         </div>
                     <button className="copy-button">Copy</button>
                     </div>
-                    <span className="see-more">More</span>
+                    <Link to="/" onClick={e=> e.stopPropagation()}>
+                        <span className="see-more">More</span>
+                    </Link>
                 </div>
             </CopyToClipboard>
         )
