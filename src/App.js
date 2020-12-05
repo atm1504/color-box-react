26f6a80687c88f973d446be6be07454ab35af4a4
Amartya Mondal <atm1504.in@gmail.com>
2020-12-05 11:01:01 +0530

    palette list

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { Route, Switch } from "react-router-dom";
 import './App.css';
 import Palette from "./Palette";
 import seedColors from "./seedColors";
-
-import { generatePalette} from "./colorHelpers";
+import { generatePalette } from "./colorHelpers";
+import PaletteList from "./PaletteList";
 // import { Route } from 'react-router-dom';
 class App extends Component{
   findPalette(id) {
@@ -15,7 +15,7 @@ class App extends Component{
   render() {
     return (
     <Switch>
-      <Route exact path="/" render={()=><h1>Palette list goes here</h1>} />
+        <Route exact path="/" render={() => <PaletteList palettes={seedColors}/>} />
       <Route exact path="/palette/:id" render={(routeProps)=><Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))}/>} />
       {/* <div>
         <Palette palette={generatePalette(seedColors[4])}/>
---
+++ src/PaletteList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+ 
+export default class PaletteList extends Component {
+    render() {
+        const { palettes } = this.props;
+         return (
+             <div>
+                 <h1>React Colors</h1>
+                 {palettes.map(pa => (
+                    <p>
+                        <Link to={`/palette/${pa.id}`}>
+                            {pa.paletteName}
+                        </Link>
+                    </p>
+                 ))}
+             </div>
+         )
+     }
+ }
+ 
