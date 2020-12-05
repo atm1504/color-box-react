b4cc01f523108c4a708adb2dbb560dab12aec42c
Amartya Mondal <atm1504.in@gmail.com>
2020-12-05 10:08:25 +0530

    Routing using id

--- src/App.js
+++ src/App.js
@@ -6,17 +6,23 @@ import seedColors from "./seedColors";
 
 import { generatePalette} from "./colorHelpers";
 // import { Route } from 'react-router-dom';
-function App() {
-  console.log(generatePalette(seedColors[4]));
-  return (
+class App extends Component{
+  findPalette(id) {
+    return seedColors.find(function (palette) {
+      return palette.id === id
+    });
+  }
+  render() {
+    return (
     <Switch>
       <Route exact path="/" render={()=><h1>Palette list goes here</h1>} />
-      <Route exact path="/palette/:id" render={()=><h1>Individual palette</h1>} />
+      <Route exact path="/palette/:id" render={(routeProps)=><Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))}/>} />
       {/* <div>
         <Palette palette={generatePalette(seedColors[4])}/>
         </div> */}
     </Switch>
-  );
+    );
+  }
 }
 
 export default App;
