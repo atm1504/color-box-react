ed595c76ff3242f538f80c32ee13cf8b4be66b6e
Amartya Mondal <atm1504.in@gmail.com>
2020-12-07 23:49:10 +0530

    Remove and refactorr css

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,9 +7,11 @@ import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
 import "rc-slider/assets/index.css";
 import Slider from 'rc-slider';
-import "./Navbar.css";
+// import "./Navbar.css";
+import { withStyles } from "@material-ui/styles";
+import styles from "./styles/NavbarStyles";
 
-export default class Navbar extends Component {
+class Navbar extends Component {
     constructor(props) {
         super(props);
         this.state = { format: "hex", open:false };
@@ -29,25 +31,25 @@ export default class Navbar extends Component {
         this.setState({ open: false });
     }
     render() {
-        const { level, changeLevel, showingAllColors } = this.props;
+        const { level, changeLevel, showingAllColors, classes } = this.props;
         const { format } = this.state;
         return (
-            <header className="Navbar">
-                <div className="logo">
+            <header className={classes.Navbar}>
+                <div className={classes.logo}>
                     <Link to="/">reactcolorpicker</Link>
                 </div>
-                {showingAllColors && (<div className="slider-container">
+                {showingAllColors && (<div className={classes.sliderContainer}>
                     <span>Level: {level}</span>
-                    <div className="slider">
+                    <div className={classes.slider}>
                         <Slider defaultValue={level} min={100} max={900} onAfterChange={changeLevel} step={100} />
                     </div>
                 </div>)}
-                <div className="select-container" >
+                <div className={classes.selectContainer} >
                     <Select onChange={this.handleFormatChange} value={format}>
                         <MenuItem value="hex">HEX - #ffffff</MenuItem>
                         <MenuItem value="rgb">RGB - rgb(255,255,255,255)</MenuItem>
                         <MenuItem value="rgba">RGBA - rgba(255,255,255,1.0)</MenuItem>
-                    </Select>
+                    </Select> 
                 </div>
                 <Snackbar
                     anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
@@ -65,3 +67,6 @@ export default class Navbar extends Component {
         )
     }
 }
+
+
+export default withStyles(styles)(Navbar);
