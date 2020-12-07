f6be763011953472db340609dbfd75cfaa6a2dc3
Amartya Mondal <atm1504.in@gmail.com>
2020-12-07 18:38:56 +0530

    feat: Add back color

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,5 +1,6 @@
 import { Palette } from '@material-ui/icons';
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 import ColorBox from "./ColorBox";
 import Navbar from "./Navbar";
 import PaletteFooter from "./PaletteFooter";
@@ -28,21 +29,25 @@ class SingleColorPalette extends Component {
     }
     render() {
         const { format } = this.state;
-        const { paletteName, emoji } = this.props.palette;
+        const { paletteName, emoji, id } = this.props.palette;
         const colorBoxes = this._shades.map(color => {
             return <ColorBox
-                key={color.id}
+                key={color.name}
                 name={color.name}
                 background={color[format]}
                 showLink={false}
             />
         })
         return (
-            <div className="Palette">
+            <div className="SingleColorPalette Palette">
                 <Navbar handleChange={this.changeFormat} showingAllColors={ false}/>
-                <h1>Single color palette</h1>
                 <div className="Palette-colors">
                     {colorBoxes}
+                    <div className="go-back ColorBox ">
+                        <Link to={`/palette/${id}`} className='back-button'>
+                            GO BACK
+                        </Link>
+                    </div>
                 </div>
                 <PaletteFooter paletteName={paletteName } emoji={emoji }/>
             </div>
