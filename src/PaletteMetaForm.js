a33e20b9b474d2dc7b8a0eb1ffa1c2667ca0d6cc
Amartya Mondal <atm1504.in@gmail.com>
2020-12-09 13:43:12 +0530

    Install emoji

--- src/PaletteMetaForm.js
+++ src/PaletteMetaForm.js
@@ -7,7 +7,8 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
-
+import { Picker } from "emoji-mart";
+import "emoji-mart/css/emoji-mart.css";
 
 class PaletteMetaForm extends Component {
   constructor(props) {
@@ -56,7 +57,7 @@ class PaletteMetaForm extends Component {
             <DialogContentText>
               Please enter a name for your new beautiful palette. Make sure it's unique!
             </DialogContentText>
-
+              <Picker />
               <TextValidator
                 label='Palette Name'
                 value={newPaletteName}
