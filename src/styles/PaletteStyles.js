809834335719473867457fca4fd6e9bbd40d9186
Amartya Mondal <atm1504.in@gmail.com>
2020-12-10 00:47:31 +0530

    update sizes

--- src/styles/PaletteStyles.js
+++ src/styles/PaletteStyles.js
@@ -1,3 +1,4 @@
+import sizes from "./sizes";
 export default {
   Palette: {
     height: "100vh",
@@ -6,5 +7,17 @@ export default {
   },
   colors: {
     height: "90%"
+  },
+    [sizes.down("lg")]: {
+      width: "25%",
+      height: "33.3333%"
+    },
+    [sizes.down("md")]: {
+      width: "50%",
+      height: "20%"
+    },
+  [sizes.down("xs")]: {
+    width: "100%",
+    height: "10%"
   }
 };
