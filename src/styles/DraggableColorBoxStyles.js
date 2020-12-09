b8f7d377d7378a68bb3db519aaa53d8c3082fa67
Amartya Mondal <atm1504.in@gmail.com>
2020-12-10 01:03:13 +0530

    Form nav styles

--- src/styles/DraggableColorBoxStyles.js
+++ src/styles/DraggableColorBoxStyles.js
@@ -1,3 +1,4 @@
+import sizes from "./sizes";
 const styles = {
     root: {
         width: "20%",
@@ -27,6 +28,18 @@ const styles = {
     },
     deleteIcon: {
         transition: "all 0.3s ease-in-out"
+    },
+    [sizes.down("lg")]: {
+      width: "25%",
+      height: "20%"
+    },
+    [sizes.down("md")]: {
+      width: "50%",
+      height: "10%"
+    },
+    [sizes.down("sm")]: {
+        width: "100%",
+        height: "5%"
     }
 }
 
